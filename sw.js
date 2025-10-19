4dd42c0a66121103c44982e10c47573e3c49e99e
May wind <2143559893@qq.com>
2025-10-19 22:24:09 +0800

    Site updated: 2025-10-19 22:24:08

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760705543803";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760883844471";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
